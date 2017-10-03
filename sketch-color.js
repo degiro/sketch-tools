c9254b8e3a1759aea368dc3dfe48118600ce5f9b
Denys Kniazevych <webschik@gmail.com>
2017-10-03 16:33:42 +0200

    Create sketch-color.js

---
+++ sketch-color.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number} value
+ * @returns {number}
+ */
+function normalizeRgbaValue (value) {
+    return Math.round(value);
+}
+
+/**
+ * @param {{red: number, green: number, blue: number, alpha: number}} msColor
+ * @returns {[number,number,number,number]}
+ */
+exports.msColorToRgba = function (msColor) {
+    const r = normalizeRgbaValue(msColor.red * 255);
+    const g = normalizeRgbaValue(msColor.green * 255);
+    const b = normalizeRgbaValue(msColor.blue * 255);
+    const a = normalizeRgbaValue(msColor.alpha * 100);
+
+    return [r, g, b, a];
+};
+
+/**
+ * @param {number} r
+ * @param {number} g
+ * @param {number} b
+ * @param {number} a
+ * @returns {{red: number, green: number, blue: number, alpha: number}}
+ */
+exports.rgbaToMSColor = function (r, g, b, a) {
+    return {
+        red: r / 255,
+        green: g / 255,
+        blue: b / 255,
+        alpha: a / 255
+    };
+};
